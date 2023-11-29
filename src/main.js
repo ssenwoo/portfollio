c2244a43dc82b5348879ab1d27adcfc0e2e7c117
ssenwoo <148453541+ssenwoo@users.noreply.github.com>
2023-11-30 05:03:41 +0900

    JS-home

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
-// Header 투명 모드
+// 스크롤시 Header 불투명 mode 전환
+
 const headerBox = document.querySelector('.header');
 const headerHeight = headerBox.getBoundingClientRect().height;
 
+headerBox.classList.remove('header--inverted');
+
 document.addEventListener('scroll', () => {
-  if (window.scrollY > headerHeight) {
-    headerBox.classList.add('header--inverted');
-  } else {
+  if (window.scrollY < headerHeight) {
     headerBox.classList.remove('header--inverted');
+  } else {
+    headerBox.classList.add('header--inverted');
   }
+});
+
+// 스크롤시 Home 화면 투명 mode 전환
+const homeBox = document.querySelector('.home__Box');
+const homeHeight = homeBox.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  const homeOpacity = 1 - window.scrollY / homeHeight;
+  homeBox.style.opacity = homeOpacity;
 });
